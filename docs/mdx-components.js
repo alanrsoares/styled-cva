8e7a8c2ac546ed897e452df0a2e98d7ca21a858e
Alan R. Soares <alanrsoars@gmail.com>
2026-01-12 10:24:51 +1300

    feat: add tailwind integration in docs

--- docs/mdx-components.js
+++ docs/mdx-components.js
@@ -1,5 +1,6 @@
 import { useMDXComponents as getThemeComponents } from "nextra-theme-docs";
 import { Tabs } from "nextra/components";
+import { Hero } from "./components/Hero";
 
 const themeComponents = getThemeComponents();
 
@@ -7,6 +8,7 @@ export function useMDXComponents(components) {
   return {
     ...themeComponents,
     Tabs,
+    Hero,
     ...components,
   };
 }
